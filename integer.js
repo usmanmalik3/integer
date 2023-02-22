dd5dbf97c9e17f2ed1842e7eb3b3ba1b5c0bf00d
usmanmalik3 <um1021723@gmail.com>
2023-02-22 10:29:34 -0800

    run

---
+++ integer.js
@@ -0,0 +1,9 @@
+let num1 = parseInt(prompt("enter first integer"))
+let num2 = parseInt(prompt("enter second integer"))
+if(num1 > num2){
+    console.log(num1 + "is laeger")
+}else if(num2 > num1){
+    console.log(num2 + "is larger")
+}else{
+    console.log("Both integers in equal")
+}
